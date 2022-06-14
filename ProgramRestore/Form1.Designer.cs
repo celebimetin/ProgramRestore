50e1d6403feea9c2c81f8b7a0cb22fa8e4d28834
Metin Çelebi <metincelebi5534@gmail.com>
2022-06-14 11:17:22 +0300

    Add project files.

---
+++ ProgramRestore/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace ProgramRestore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDosya = new System.Windows.Forms.TextBox();
+            this.btnSec = new System.Windows.Forms.Button();
+            this.btnYukle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDosya
+            // 
+            this.txtDosya.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDosya.Location = new System.Drawing.Point(12, 12);
+            this.txtDosya.Name = "txtDosya";
+            this.txtDosya.Size = new System.Drawing.Size(428, 30);
+            this.txtDosya.TabIndex = 0;
+            // 
+            // btnSec
+            // 
+            this.btnSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSec.Location = new System.Drawing.Point(480, 12);
+            this.btnSec.Name = "btnSec";
+            this.btnSec.Size = new System.Drawing.Size(75, 30);
+            this.btnSec.TabIndex = 1;
+            this.btnSec.Text = "Seç";
+            this.btnSec.UseVisualStyleBackColor = true;
+            this.btnSec.Click += new System.EventHandler(this.btnSec_Click);
+            // 
+            // btnYukle
+            // 
+            this.btnYukle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnYukle.Location = new System.Drawing.Point(480, 48);
+            this.btnYukle.Name = "btnYukle";
+            this.btnYukle.Size = new System.Drawing.Size(75, 43);
+            this.btnYukle.TabIndex = 2;
+            this.btnYukle.Text = "Yükle";
+            this.btnYukle.UseVisualStyleBackColor = true;
+            this.btnYukle.Click += new System.EventHandler(this.btnYukle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Bu işlemi yaptığınız zaman mevcut veriler silinecek\r\nyedeğinizdeki veriler yüklen" +
+    "ecektir.!!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 103);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnYukle);
+            this.Controls.Add(this.btnSec);
+            this.Controls.Add(this.txtDosya);
+            this.MaximumSize = new System.Drawing.Size(600, 150);
+            this.MinimumSize = new System.Drawing.Size(600, 150);
+            this.Name = "Form1";
+            this.Text = "YEDEK YÜKLEME PROGRAMI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDosya;
+        private System.Windows.Forms.Button btnSec;
+        private System.Windows.Forms.Button btnYukle;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProgramRestore/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ProgramRestore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Veri dosyasını seçiniz|*.bak";
+            openFile.ShowDialog();
+            txtDosya.Text = openFile.FileName;
+        }
+
+        private void btnYukle_Click(object sender, EventArgs e)
+        {
+            if (txtDosya.Text != "")
+            {
+                try
+                {
+                    string strSql = @""; // burası yapılacak eksik
+                    Cursor.Current = Cursors.WaitCursor;
+                    string yedekYolu = txtDosya.Text;
+                    Application.DoEvents();
+                    string str = Application.StartupPath + @""; // burası yapılacak eksik
+
+                    using (SqlConnection connection = new SqlConnection(strSql))
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand(@"USE Master; If Exists(Select * From sys.databases where name='BarcodeSalesDb') Drop Database[" + str + "]; RESTORE DATABASE[" + str + "] FROM DISK=N'" + txtDosya.Text + "'", connection);
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+
+                    MessageBox.Show("Veriler yüklenmiştir.");
+                    Process.Start(Application.StartupPath + "\\BarcodeSales.exe");
+                    Cursor.Current = Cursors.Default;
+                    Application.Exit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
